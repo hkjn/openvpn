b6906714fea92cbf71196af3a50233d6a9401bbf
Henrik Jonsson <me@hkjn.me>
2016-07-05 18:32:14 +0300

    build.sh: Tag IMAGE with CPU arch as well

--- build.sh
+++ build.sh
@@ -5,15 +5,17 @@
 set -euo pipefail
 
 BUILD_DIR="$(mktemp -d)"
-IMAGE="hkjn/openvpn"
 cp Dockerfile "$BUILD_DIR/"
 cp run.sh "$BUILD_DIR/"
 ARCH="$(uname -m)"
+IMAGE="hkjn/openvpn"
 [[ $ARCH = "x86_64" ]] || {
   # TODO(hkjn): Improve this special-casing to support different CPU
   # architectures. Just call it hkjn/x86_64-alpine as well?
-  sed -i "s/FROM hkjn\//FROM hkjn\/$(uname -m)-/" "$BUILD_DIR/Dockerfile"
+  sed -i "s/FROM hkjn\//FROM hkjn\/$ARCH-/" "$BUILD_DIR/Dockerfile"
+  IMAGE="hkjn/ARCH-openvpn"
 }
+
 docker build -t $IMAGE "$BUILD_DIR/"
 NO_PUSH=${NO_PUSH:-""}
 [[ "$NO_PUSH" ]] || docker push $IMAGE
